4fc6dc528ab4eb46f32b2d248680ae0fd5b6ea35
Guilherme Palazzo <palazzoeng1@gmail.com>
2023-03-02 15:32:00 -0300

    adding volatility-based labeling technique

--- src/crypto_thesis/data_domains/spine/build_labels.py
+++ src/crypto_thesis/data_domains/spine/build_labels.py
@@ -8,7 +8,7 @@ import pandas as pd
 def spine_build_target_labels(df: pd.DataFrame, df_log_ret: pd.DataFrame, label_params: Dict[str, float]) -> pd.DataFrame:
 
     std_df = _build_stdev_by_window(df=df, df_log_ret=df_log_ret)
-    final_df = df.merge(std_df, on=["open_time", "close_time"], how="inner")
+    final_df = df.merge(std_df, on=["open_time", "target_time"], how="inner")
 
     assert df.shape[0] == final_df.shape[0], "Data loss, please review."
 
@@ -18,7 +18,7 @@ def spine_build_target_labels(df: pd.DataFrame, df_log_ret: pd.DataFrame, label_
 
     final_df.loc[:, "label"] = final_df.apply(lambda col: "top" \
                                                 if col["close_to_tgt_time_logret"] >= \
-                                                    col["logret_cumsum"] * col["std"] * (1 + label_params["tau"]) \
+                                                    col["logret_cumsum"] + col["std"] * label_params["tau"] \
                                                 else "bottom" \
                                             , axis=1)
 
@@ -29,11 +29,11 @@ def _build_stdev_by_window(df: pd.DataFrame, df_log_ret: pd.DataFrame) -> pd.Dat
 
     std_df = pd.DataFrame()
 
-    for start, end in zip(df["open_time"], df["close_time"]):
+    for start, end in zip(df["open_time"], df["target_time"]):
         df_filter = df_log_ret[df_log_ret["close_time"].between(start, end)]
 
         _std = df_filter["log_return"].std(ddof=1) #sampled stdev
-        df_aux = pd.DataFrame({"open_time": start, "close_time": end, "std": _std}, index=[0])
+        df_aux = pd.DataFrame({"open_time": start, "target_time": end, "std": _std}, index=[0])
 
         std_df = pd.concat([std_df, df_aux])
 
