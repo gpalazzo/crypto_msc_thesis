1ab785ec462ce5a1612cc5f5b5a5e2b564503c75
Guilherme Palazzo <palazzoeng1@gmail.com>
2023-03-27 18:07:42 -0300

    adding stdev labeling method

--- src/crypto_thesis/data_domains/spine/build_labels.py
+++ src/crypto_thesis/data_domains/spine/build_labels.py
@@ -14,9 +14,13 @@ def spine_build_target_labels(df: pd.DataFrame,
                             class_bounds: Dict[str, float]) -> pd.DataFrame:
 
     std_df = _build_stdev_by_window(df=df, df_log_ret=df_log_ret)
-    final_df = df.merge(std_df, on=["open_time", "target_time"], how="inner")
+    final_df = df.merge(std_df, on=["open_time", "close_time"], how="inner")
 
     assert df.shape[0] == final_df.shape[0], "Data loss, please review."
+
+    # it has nulls for rows were the volume is equal to the volume bar size
+    # so there's only data point and therefore there's no stdev
+    final_df = final_df.dropna()
 
     # build log return between close_time price and target_time price
     final_df.loc[:, "close_to_tgt_time_logret"] = np.log(final_df["target_time_close"] \
@@ -49,11 +53,11 @@ def _build_stdev_by_window(df: pd.DataFrame, df_log_ret: pd.DataFrame) -> pd.Dat
 
     std_df = pd.DataFrame()
 
-    for start, end in zip(df["open_time"], df["target_time"]):
+    for start, end in zip(df["open_time"], df["close_time"]):
         df_filter = df_log_ret[df_log_ret["close_time"].between(start, end)]
 
         _std = df_filter["log_return"].std(ddof=1) #sampled stdev
-        df_aux = pd.DataFrame({"open_time": start, "target_time": end, "std": _std}, index=[0])
+        df_aux = pd.DataFrame({"open_time": start, "close_time": end, "std": _std}, index=[0])
 
         std_df = pd.concat([std_df, df_aux])
 
