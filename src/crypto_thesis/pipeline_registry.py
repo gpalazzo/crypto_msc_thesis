cb1a521ec414868f13fe89588050e77ae997f953
gpalazzo <gpalazzo93@icloud.com>
2023-05-02 08:52:31 -0300

    adding new deps

--- src/crypto_thesis/pipeline_registry.py
+++ src/crypto_thesis/pipeline_registry.py
@@ -6,7 +6,7 @@ from kedro.pipeline import Pipeline, pipeline
 
 from crypto_thesis.pipelines.benchmark import benchmark_pipeline
 from crypto_thesis.pipelines.binance import binance_pipeline
-from crypto_thesis.pipelines.manual_input import manual_input_pipeline
+# from crypto_thesis.pipelines.manual_input import manual_input_pipeline
 from crypto_thesis.pipelines.master_table import master_table_pipeline
 from crypto_thesis.pipelines.modeling import ml_models_pipeline
 from crypto_thesis.pipelines.portfolio import portfolio_pipeline
@@ -20,9 +20,9 @@ def register_pipelines() -> Dict[str, Pipeline]:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
     return {"__default__": pipeline([binance_pipeline() +
-                                    manual_input_pipeline() +
+                                    # manual_input_pipeline() +
                                     spine_pipeline() +
                                     master_table_pipeline() +
-                                    benchmark_pipeline() +
+                                    # benchmark_pipeline() +
                                     ml_models_pipeline() +
                                     portfolio_pipeline()])}
