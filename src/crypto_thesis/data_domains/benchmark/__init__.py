0fd497bdcfefdd2b5baa72c11ee6f5342e288f70
gpalazzo <gpalazzo93@icloud.com>
2023-05-08 17:33:38 -0300

    adding new data set for benchmark

--- src/crypto_thesis/data_domains/benchmark/__init__.py
+++ src/crypto_thesis/data_domains/benchmark/__init__.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-from .build_strategies import buy_and_hold_strategy, trend_following_strategy, pc1_index_strategy
+from .build_strategies import buy_and_hold_strategy, trend_following_strategy
