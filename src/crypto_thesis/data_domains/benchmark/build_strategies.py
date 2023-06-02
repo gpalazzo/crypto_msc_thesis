72f9ba2d20d93ab38424256d6bdcf5be886e6843
gpalazzo <gpalazzo93@icloud.com>
2023-06-02 09:26:44 -0300

    test

--- src/crypto_thesis/data_domains/benchmark/build_strategies.py
+++ src/crypto_thesis/data_domains/benchmark/build_strategies.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 from crypto_thesis.utils import mt_split_train_test
 
+abc = ""
+
 TARGET_COL = ["label"]
 # these cols were useful so far, but not anymore
 INDEX_COL = "window_nbr"
