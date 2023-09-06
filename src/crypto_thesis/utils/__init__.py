5a38fa44c16bebacb03a2c781ec779bc7d8a0e4a
gpalazzo <gpalazzo93@icloud.com>
2023-09-06 13:46:24 -0300

    removing legacy function

--- src/crypto_thesis/utils/__init__.py
+++ src/crypto_thesis/utils/__init__.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
 from .commons import build_log_return, build_timeseries
 from .features import apply_mic_fte_slct, apply_vif_fte_slct
-from .models import (
-    mt_split_train_test,
-    optimize_params,
-    scale_train_test,
-    split_window_nbr,
-)
+from .models import mt_split_train_test, optimize_params, scale_train_test
