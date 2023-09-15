06ac4507d7feb21d17c38d8094890b6e6d4064d7
gpalazzo <gpalazzo93@icloud.com>
2023-09-15 09:56:44 -0300

    add lstm params opt

--- src/crypto_thesis/data_domains/modeling/xgboost.py
+++ src/crypto_thesis/data_domains/modeling/xgboost.py
@@ -47,7 +47,7 @@ def xgboost_model_fit(master_table_train: pd.DataFrame,
                                     grid=model_params,
                                     X_train=X_train,
                                     y_train=y_train,
-                                    n_splits=10)
+                                    n_splits=5)
         params_opt = params_opt.best_params_
 
     else:
